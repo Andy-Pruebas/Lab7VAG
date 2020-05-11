f47b6c31f5d7de08bb0bdb863b029a76c9416404
Andy-Pruebas <andy.vilca@tecsup.edu.pe>
2020-05-11 15:31:03 -0500

    Primer Commit

---
+++ Dependencias/Dependencias/Dependencias.Android/TextToSpeechImplementation.cs
@@ -0,0 +1,39 @@
+﻿using Android.Speech.Tts;
+using Dependencias.Droid;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(TextToSpeechImplementation))]
+namespace Dependencias.Droid
+{
+
+    public class TextToSpeechImplementation : Java.Lang.Object, ITextToSpeech, TextToSpeech.IOnInitListener
+    {
+        TextToSpeech speaker;
+        string toSpeak;
+
+        [System.Obsolete]
+        public void Speak(string text)
+        {
+            toSpeak = text;
+            if (speaker == null)
+            {
+
+                speaker = new TextToSpeech(Forms.Context, this);
+            }
+            else
+            {
+                speaker.Speak(toSpeak, QueueMode.Flush, null, null);
+            }
+        }
+
+        #region IOnInitListener implementation
+        public void OnInit(OperationResult status)
+        {
+            if (status.Equals(OperationResult.Success))
+            {
+                speaker.Speak(toSpeak, QueueMode.Flush, null, null);
+            }
+        }
+        #endregion
+    }
+}
